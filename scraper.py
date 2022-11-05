13ad8b5802488b40b4cb8a49cdcf9ad6c828658d
mmillet88 <m.kosyuchenko@gmail.com>
2022-11-05 14:23:04 +0000

    Creates a Scraper class.

---
+++ scraper.py
@@ -0,0 +1,40 @@
+from selenium import webdriver
+from time import sleep
+import time
+from selenium.webdriver.common.by import By
+
+class Scraper:
+
+    def __init__(self, url):
+        self.url = url
+        self.driver = webdriver.Chrome() 
+
+    def get_start_price(self):
+        pass
+
+    def get_all_prices(self):
+        pass
+
+    def get_links(self):
+        pass
+
+    def get_location(self):
+        pass
+
+    def get_bedrooms(self):
+        pass
+    
+    def get_bathrooms(self):
+        pass
+
+    def get_description(self):
+        pass
+
+    def get_equip_fac_events(self):
+        pass
+
+    def get_location(self):
+        pass
+
+    def get_distance_from(self):
+        pass
